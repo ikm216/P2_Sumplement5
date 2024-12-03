2d4a74dd71b23954ab05c564c68fabe3dea34347
ikm216 <ikmclassof22@gmail.com>
2024-12-02 20:47:21 -0500

    test:added test to check if it is a 2d matrix is a magic square

--- test.java
+++ test.java
@@ -1,5 +1,9 @@
 import org.junit.*;
 
 public class test {
-    
+    @Test
+    public void test_should_return_Magic_Squares(){
+        int [][] magic = {{8, 1, 4}, {3, 3, 5}, {6, 9, 2}};
+        Assert.assertTrue(su.magicSquares(magic));
+    }
 }
