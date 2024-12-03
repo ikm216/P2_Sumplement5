c86e46ee17b5081a56f52ac554cfb37c7fdc5c7b
ikm216 <ikmclassof22@gmail.com>
2024-12-02 21:14:46 -0500

    feat:added feat to check if it is a 2d matrix is a magic square

--- su.java
+++ su.java
@@ -1,3 +1,40 @@
 public class su{
-    
+    public static boolean magicSquares(int[][] magic) {
+        int n = magic.length;
+        if (n == 0 || magic[0].length != n){
+            return false; 
+
+        }
+        int sum = 0;
+        for (int val : magic[0]){
+            sum += val;
+        } 
+
+        for (int i = 0; i < n; i++) {
+            int row_sum = 0;
+            for (int j = 0; j < n; j++) {
+                row_sum += magic[i][j];
+            }
+            if (row_sum != sum){
+                return false;
+            }
+        }
+
+        // Check columns
+        for (int j = 0; j < n; j++) {
+            int col_sum = 0;
+            for (int i = 0; i < n; i++) {
+                col_sum += magic[i][j];
+            }
+            if (col_sum != sum) return false;
+        }
+
+        // Check diagonals
+        int diag1 = 0, diag2 = 0;
+        for (int i = 0; i < n; i++) {
+            diag1 += magic[i][i];
+            diag2 += magic[i][n - i - 1];
+        }
+        return diag1 == sum && diag2 == sum;
+    }
 }
